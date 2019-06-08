1074831a0ff9c2af2015e7e0e5b9995c2c62457f
yu2000324 <46483171+yu2000324@users.noreply.github.com>
2019-06-08 23:38:03 +0800

    Add files via upload

---
+++ 冒泡排序.cpp
@@ -0,0 +1,29 @@
+#include"stdio.h"
+constexpr auto N = 10;
+int main()
+{
+	int i, j, tmp;
+	int Bubble[N];
+	printf_s("请输入十个数:");
+	for (i = 0; i < N; i++)
+	{
+		scanf_s("%d", &Bubble[i]);
+	}
+	for (i = 0; i < N-1; i++)
+	{
+		for (j = i; j <N-1; j++)
+		{
+			if (Bubble[j] > Bubble[j + 1])
+			{
+				tmp = Bubble[j];
+				Bubble[j] = Bubble[j + 1];
+				Bubble[j + 1] = tmp;
+			}
+		}
+	}
+	for(i=0;i<N;i++)
+	{
+		printf_s("%d ", Bubble[i]);
+	}
+	
+}
